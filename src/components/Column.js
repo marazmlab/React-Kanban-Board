7cac17a4f4cb202b5f77d1e09654b44428af6cf9
Marek Bełz <belzmarek@gmail.com>
2025-04-18 11:39:21 +0000

    styles fix

--- src/components/Column.js
+++ src/components/Column.js
@@ -4,9 +4,18 @@ import Task from './Task';
 import '../styles/components-styles/Column.css';
 
 function Column({ id, name, tasks }) {
+
+  const titleClass = `column__title ${
+    id === 1
+      ? 'column__title--pending'
+      : id === 2
+      ? 'column__title--doing'
+      : 'column__title--done'
+  }`;
+
   return (
     <div className='column'>
-      <h3 className='column__title'>{name}</h3>
+      <h3 className={titleClass}>{name}</h3>
       <ul className='column__tasks'>
         {tasks.map((task) => (
           <Task key={task.id} id={task.id} name={task.name} user={task.user} columnId={id} />

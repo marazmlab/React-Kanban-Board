fba2f6ad31328c659655601d4da8cd21f7db1f1a
Marek Bełz <marazm.lab@gmail.com>
2025-07-10 20:22:46 +0000

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './components/Board';
+import Header from "./components/Header";
 import { BoardProvider } from './components/BoardContext';
 
 function App() {
   return (
     <BoardProvider>
+      <Header />
       <Board />
     </BoardProvider>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/components-styles/Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            React Kanban Board
+        </header>
+    );
+}
+
+export default Header;
